a1af41f98efd4ff7f755d80c2581e20c8b99b8b4
sarra rif <stage.sarra.rif@gmail.com>
2023-08-01 18:06:14 +0200

    first commit

---
+++ boucle.py
@@ -0,0 +1,8 @@
+# Avec Python, c’est très facile de créer des boucles. 
+# Si vous voulez afficher tous les éléments dans une liste, le code ressemblera à ça :
+
+print ("\n j'affiche la liste des races de chiens \n ")
+races_de_chien = ["golden retriever", "chihuahua", "terrier", "carlin"]
+for chien in races_de_chien:
+    print(chien)
+    
